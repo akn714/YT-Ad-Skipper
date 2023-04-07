da0f531d76e6f2a5c66a005b9959f97fa9da24cf
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-07 15:21:22 +0530

    added background.js and updated content.js and manifest.json

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,13 +1,31 @@
-setInterval(() => {
-  let div = document.createElement("div");
-  div.setAttribute("class", "yt-ad-skipper-div");
-  div.innerText = "yt-ad-skipper is activated";
-  div.setAttribute(
-    "style",
-    "background: #0064ff5e; color: white; position: fixed; top: 20px; right: 40px; height: 50px; width: 210px; z-index: 10000;font-size: 15px;display: flex;justify-content: center;align-items: center;border: 1px solid blue;border-radius: 11px;"
-  );
-  document.body.appendChild(div);
+let div = document.createElement("div");
+div.setAttribute("class", "yt-ad-skipper-div");
+div.innerText = "yt-ad-skipper is activated";
+div.setAttribute(
+  "style",
+  `background: rgba(95, 95, 95, 0.37);
+            color: rgb(175, 175, 175);
+            position: fixed;
+            top: 20px;
+            right: 40px;
+            height: 50px;
+            width: 210px;
+            z-index: 10000;
+            font-size: 15px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid rgb(159, 159, 159);
+            border-radius: 11px;
+            backdrop-filter: blur(2px);`
+);
+document.body.appendChild(div);
 
+setTimeout(() => {
+  div.style.display = "none";
+}, 6000);
+
+setInterval(() => {
   let video = document.querySelectorAll(".video-stream");
   if (document.querySelector("div.ad-showing")) {
     if (document.querySelector(".ytp-ad-skip-button")) {
@@ -16,5 +34,4 @@ setInterval(() => {
       video[0].currentTime = video[0].duration;
     }
   }
-  console.log("checking");
 }, 1000);
