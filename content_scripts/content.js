d966adcb90bc0359a76c5f9b5e2659a49ef006f2
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-07 12:57:20 +0530

    Update content.js

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,12 +1,20 @@
-setInterval(()=>{
-    let video = document.querySelectorAll('.video-stream');
-    if(document.querySelector('div.ad-showing')){
-        if(document.querySelector('.ytp-ad-skip-button')){
-            document.querySelector('.ytp-ad-skip-button').click();
-        }
-        else{
-            video[0].currentTime = video[0].duration;
-        }
+setInterval(() => {
+  let div = document.createElement("div");
+  div.setAttribute("class", "yt-ad-skipper-div");
+  div.innerText = "yt-ad-skipper is activated";
+  div.setAttribute(
+    "style",
+    "background: #0064ff5e; color: white; position: fixed; top: 20px; right: 40px; height: 50px; width: 210px; z-index: 10000;font-size: 15px;display: flex;justify-content: center;align-items: center;border: 1px solid blue;border-radius: 11px;"
+  );
+  document.body.appendChild(div);
+
+  let video = document.querySelectorAll(".video-stream");
+  if (document.querySelector("div.ad-showing")) {
+    if (document.querySelector(".ytp-ad-skip-button")) {
+      document.querySelector(".ytp-ad-skip-button").click();
+    } else {
+      video[0].currentTime = video[0].duration;
     }
-    console.log('checking')
-}, 1000)
+  }
+  console.log("checking");
+}, 1000);
